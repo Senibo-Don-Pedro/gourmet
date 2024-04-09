2e5a41ede3b7ec2c3c9b05387b3b606b564b9294
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-04-09 19:25:54 +0100

    adding user profile form

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantpage from "./pages/ManageRestaurantPage"
 
 const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AppRoutes = () => {
       <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
       <Route element={<ProtectedRoute/>}>
         <Route path="/user-profile" element={<Layout ><UserProfilePage/></Layout>}/>
+        <Route path="/manage-restaurant" element={<Layout ><ManageRestaurantpage/></Layout>}/>
       </Route>
       <Route path="*" element={<Navigate to={"/"}/>}/>
     </Routes>
--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,14 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import { Loader2 } from "lucide-react"
 import { Navigate, Outlet } from "react-router-dom"
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth0()
+  const { isAuthenticated, isLoading } = useAuth0()
+  
+  if (isLoading) {
+    return (
+      <div className=" h-screen flex justify-center items-center -mt-20">
+        <Loader2 color="orange" className="w-10 h-10 animate-spin" />
+      </div>
+    )
+  }
 
-  return isAuthenticated ? (
+  if (isAuthenticated) {
+    return <Outlet/>
+  }
+
+  return <Navigate to="/" replace />
+  
+  /*return isAuthenticated ? (
     <Outlet/>
   ) : (
     <Navigate to="/" replace />
-  )
+  ) */
 }
 
 export default ProtectedRoute
