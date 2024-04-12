f1efd946dd3480b0b7d5aa2bd9304cd082e96e0a
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-04-12 11:01:25 +0100

    added search page and filters

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,12 +5,14 @@ import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantpage from "./pages/ManageRestaurantPage"
+import SearchPage from "./pages/SearchPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout showhero ><HomePage/></Layout>}/>
       <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+      <Route path="/search/:city" element={<Layout showhero={false}><SearchPage/></Layout>} />
       <Route element={<ProtectedRoute/>}>
         <Route path="/user-profile" element={<Layout ><UserProfilePage/></Layout>}/>
         <Route path="/manage-restaurant" element={<Layout ><ManageRestaurantpage/></Layout>}/>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate()
 
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`
+    })
+  }
 
   return (
     <div className="flex flex-col gap-12">
@@ -11,6 +19,7 @@ const HomePage = () => {
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar placeHolder="Search by city or town" onSubmit={handleSearchSubmit}  />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
