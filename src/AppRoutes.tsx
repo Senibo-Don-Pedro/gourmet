40c38610f3ee60cfc44768a55d929d2b1a5776a8
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-04-13 22:31:06 +0100

    starting the stripe process

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantpage from "./pages/ManageRestaurantPage"
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Layout showhero ><HomePage/></Layout>}/>
       <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
       <Route path="/search/:city" element={<Layout showhero={false}><SearchPage/></Layout>} />
+      <Route path="/detail/:restaurantId" element={<Layout showhero={false}><DetailPage/></Layout>} />
       <Route element={<ProtectedRoute/>}>
         <Route path="/user-profile" element={<Layout ><UserProfilePage/></Layout>}/>
         <Route path="/manage-restaurant" element={<Layout ><ManageRestaurantpage/></Layout>}/>
