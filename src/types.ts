4fa59c7de3d5110fcee07cd8eaa6fe47c946dbb7
Senibo-Don-Pedro <senibodonpedro@gmail.com>
2024-04-24 15:07:25 +0100

    checkout flow

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
 export type User = {
-  _id: string
-  email: string
-  name: string
-  addressLine1: string
-  city: string
-  country: string
-}
+  _id: string;
+  email: string;
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
 
 export type MenuItem = {
   _id: string;
@@ -27,12 +27,39 @@ export type Restaurant = {
   lastUpdated: string;
 };
 
-export type RestaurantSearchResponse = {
-  data: Restaurant[]
-  pagination: {
-    total: number
-    page: number
-    pages: number
-  }
-}
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
 
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
+};
+
+export type RestaurantSearchResponse = {
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
+};
